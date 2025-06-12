bd89e7ced430798e356ee03e01a866784e81ea07
Yeongeunn <nye0817@gmail.com>
2025-06-12 20:33:44 +0900

    Refactor: 불필요한 주석 제거

--- routine/urls.py
+++ routine/urls.py
@@ -3,16 +3,6 @@ from . import views
 
 app_name = 'routines' # 앱 네임스페이스 설정
 
-# urlpatterns = [
-#     path('', views.save_routine, name='routine_home'),     
-#     path('api/routines/', views.save_routine, name='save_routine'),  # POST 루틴 저장
-#     path('api/routines/events/', views.get_routine_events, name='get_routine_events'),  # GET 루틴 목록
-#     path('routine/delete/<int:pk>/', views.delete_routine, name='delete_routine'), # DELETE 루틴 삭제
-#     #path("api/routines/today/", views.get_today_routines, name="get_today_routines"),
-#     #path("api/routines/today/", views.test_routines_for_scheduler), #테스트용!!!!
-#     path('today/', views.test_routines_for_scheduler), # 테스트용 루틴 조회
-# ]
-
 #api/routines/ 기준으로 상대 경로
 
 urlpatterns = [
